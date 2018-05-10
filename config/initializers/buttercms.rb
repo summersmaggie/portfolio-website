f985897a2676e78a553bc435a115bba961658198
Guest User <Guest@Epicodus-3E.local>
2018-05-10 14:00:41 -0700

    add styling to blog section

--- config/initializers/buttercms.rb
+++ config/initializers/buttercms.rb
@@ -2,7 +2,10 @@ require 'buttercms-ruby'
 
 # If you added the Heroku Butter add-on, ENV["BUTTER_TOKEN"] will be defined.
 # Otherwise, grab your token at https://buttercms.com/profile/ and set it below
-ButterCMS::api_token = ENV['BUTTER_TOKEN']
+ButterCMS::api_token='b53c09103f6dcb7b7f3009c6b268204d4c10dde0'
+
+#
+# ENV['BUTTER_TOKEN']
 
 # Test mode can be used to setup a staging website for previewing content or for testing content during local development.
 # ButterCMS::test_mode = true
