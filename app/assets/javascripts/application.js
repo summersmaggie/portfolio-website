bdd0ca627beff553adc96c18ba52e1635e84851f
Maggie Summers <summers.maggie@gmail.com>
2018-09-11 14:26:24 -0700

    add colors and jquery to change on refresh

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,4 +18,10 @@ $(document).ready(function(){
   $('.current_list').live('click', function() {
       $('#current_interests').toggle();
   });
+
+  var colors = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000'];
+
+  var rand = Math.floor(Math.random() * colors.length );
+
+  $('#intro').css("color", colors[rand]);
 });
