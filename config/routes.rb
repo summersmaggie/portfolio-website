862dc27af812cb0f9a15fb70add87f86dd128afd
Maggie Summers <summers.maggie@gmail.com>
2018-09-11 14:16:38 -0700

    add new homepage to website, add styling and responsiveness

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,8 @@ Rails.application.routes.draw do
 
   resources :skills
 
+  resources :about, only: [:index]
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
